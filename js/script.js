e7dfdf85c28b0cc4305b69a565b240d5e6928760
150Victor150 <victor.silva.oliveira@escola.pr.gov.br>
2025-06-30 10:59:53 -0300

    inicializei o projeto

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function logar(){
+    const email = document.getElementById("email").value;
+    console.log (email);
+
+    const senha = document.getElementById("senha").value;
+    console.log (senha);
+
+    if (senha == "123"){
+        alert ("Oi meu lindo professor Noberto");
+    }
+
+
+}
